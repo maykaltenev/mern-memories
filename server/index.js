bf14e5de749cef9f6ed89efdb60ccf7b33d79049
Maykal Tenev <michaeltenev@gmail.com>
2022-10-21 21:42:58 +0200

    setting up start

--- server/index.js
+++ server/index.js
@@ -3,9 +3,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
-
+dotenv.config();
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
@@ -14,11 +15,11 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
-const PORT = process.env.PORT|| 5000;
+const CONNECTION_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .then(() => console.log("Database is connected! 🍀"))
   .catch((error) => console.log(`${error} did not connect`));
-
+app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`))
 mongoose.set('useFindAndModify', false);
