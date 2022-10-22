a6c433b7bbdb0ecf7a4c5075c42c4b42b93df2b5
Maykal Tenev <michaeltenev@gmail.com>
2022-10-22 22:12:05 +0200

    remove creator

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,7 +1,6 @@
 import { AUTH, LOGOUT } from '../constants/actionTypes'
 
 const authReducer = (state = { authData: null }, action) => {
-
     switch (action.type) {
         case AUTH:
             localStorage.setItem('profile', JSON.stringify({ ...action?.data }));
